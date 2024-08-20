95527ec929cefed89a6f5e42012f001fa9d24edb
your name <eli.pousson@gmail.com>
2024-08-20 11:50:03 -0400

    Update data

--- data-raw/works.R
+++ data-raw/works.R
@@ -1,6 +1,38 @@
 ## code to prepare `works` dataset goes here
 library(dplyr)
+library(sf)
 # works <- mapbaltimore::public_art
+
+baltimore_tracts <- tigris::tracts("MD", c("Baltimore city", "Baltimore County"))
+
+baltimore_tracts <- baltimore_tracts |>
+  select(
+    tract = NAMELSAD,
+    tract_geoid = GEOID
+  ) |>
+  sf::st_transform(3857)
+
+csas <- arcgislayers::arc_read(
+  "https://services1.arcgis.com/mVFRs7NF4iFitgbY/arcgis/rest/services/Community_Statistical_Areas_(CSAs)__Reference_Boundaries/FeatureServer/0",
+  col_select = c("Community", "CSA2020"),
+  col_names = c("csa", "csa_2020")
+)
+
+csas <- csas |>
+  st_transform(3857) |>
+  st_make_valid(
+    geos_method = "valid_linework"
+  )
+
+legislative_districts <- dplyr::select(mapbaltimore::legislative_districts, legislative_district = name) |>
+  sf::st_transform(3857)
+
+
+neighborhoods <- dplyr::select(mapbaltimore::neighborhoods, neighborhood = name) |>
+  sf::st_transform(3857)
+
+council_districts <- dplyr::select(mapbaltimore::council_districts, council_district = name) |>
+  sf::st_transform(3857)
 
 url <- "https://airtable.com/apprU9l3Ca60s60Pa/tblzkmvNZcqFVsRyC/viwuTHuri6z8QJ5wV?blocks=hide"
 
@@ -18,13 +50,14 @@ works_sf <- works_df |>
   janitor::clean_names() |>
   dplyr::filter(!is.na(longitude)) |>
   sf::st_as_sf(
-  crs = 4326,
-  na.fail = FALSE,
-  coords = c("longitude", "latitude")
-)
+    crs = 4326,
+    na.fail = FALSE,
+    coords = c("longitude", "latitude")
+  )
 
 works <-
   works_sf %>%
+  sf::st_transform(3857) |>
   janitor::clean_names() |>
   dplyr::select(
     id,
@@ -59,6 +92,29 @@ works <-
     geometry
   )
 
+if (FALSE) {
+  works |>
+    sf::st_join(
+      baltimore_tracts
+    ) |>
+    sf::st_join(
+      csas
+    ) |>
+    dplyr::bind_cols(
+      works |>
+        sf::st_transform(4326) |>
+        sf::st_coordinates() |>
+        as.data.frame() |>
+        set_names(c("lon", "lat"))
+    ) |>
+    dplyr::arrange(dplyr::desc(year), title) |>
+    sf::st_drop_geometry() |>
+    readr::write_csv(
+      "Baltimore-Public-Art-Inventory_Works_2024-08-20.csv"
+    )
+}
+
+
 works <-
   works %>%
   mutate(
@@ -70,21 +126,21 @@ works <-
   )
 
 works <- works %>%
-  sf::st_transform(2804) %>%
+  sf::st_transform(3857) %>%
   sf::st_join(
-    dplyr::select(mapbaltimore::csas, csa = name)
+    csas
   ) %>%
   sf::st_join(
-    dplyr::select(mapbaltimore::legislative_districts, legislative_district = name)
+    legislative_districts
   ) %>%
   sf::st_join(
-    dplyr::select(mapbaltimore::neighborhoods, neighborhood = name)
+    neighborhoods
   ) %>%
   sf::st_join(
-    dplyr::select(mapbaltimore::council_districts, council_district = name)
+    council_districts
   ) %>%
   dplyr::relocate(
-    neighborhood, csa, council_district, legislative_district,
+    neighborhood, starts_with("csa"), council_district, legislative_district,
     .before = location_desc
   ) %>%
   sf::st_transform(4326)
@@ -107,4 +163,3 @@ baltimore_city <- mapbaltimore::baltimore_city %>%
   sf::st_transform(4326)
 
 readr::write_rds(baltimore_city, "data/baltimore_city.rda")
-
